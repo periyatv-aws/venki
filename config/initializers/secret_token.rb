038f1c3722db43a41fb3b99c6dc59cf6c57954f4
cool2venki <cool2venki@gmail.com>
2012-07-12 12:48:17 +0100

    day1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Venki::Application.config.secret_token = 'ad8ab46de2b154b7a7f1ac132051db39be3064a95ffac76d38f8393f1d1d2f15ac5e9d698c3da44fcb2ce0aee80a491db623b970cd0950ba9825239eeb438ec8'
